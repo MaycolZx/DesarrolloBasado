d5b97b8b9fe2ce14ed78e64b5d87452185679019
Maycol-Zx-PC <alexander51600@hotmail.com>
2022-09-26 21:14:49 +0000

    actualizado

--- app/main.py
+++ app/main.py
@@ -6,9 +6,26 @@ app = Flask(__name__)
 def form():
     return render_template("formulario.html")
 
-@app.route("/cv")
+@app.route("/cv1")
 def curr():
     return render_template("curriculum.html")
+
+@app.route("/cv", methods=["POST"])
+def hello():
+    name = request.form.get("name")
+    fechaSN = request.form.get("fechaSN")
+    ocupacion = request.form.get("ocupacion")
+    contacto = request.form.get("contacto")
+    nacionalidad = request.form.get("nacionalidad")
+    nivelIngl = request.form.get("nivelIngl")
+    lengProg = request.form.get("lengProg")
+    aptitud = request.form.get("aptitud")
+    habilidad = request.form.get("habilidad")
+    perfil = request.form.get("perfil")
+
+    return render_template("curriculum.html", name=name,fechaSN = fechaSN, ocupacion = ocupacion,
+     contacto=contacto,nacionalidad = nacionalidad, nivelIngl = nivelIngl, lengProg = lengProg,
+     aptitud = aptitud,habilidad = habilidad, perfil = perfil )
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
